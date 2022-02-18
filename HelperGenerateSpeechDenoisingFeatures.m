40476ca1dc6c98edd53eb4f306f19084f1be7f72
YilikaLoufoua <yilika.loufoua@tcu.edu>
2022-02-18 02:50:05 -0600

    generating target and predictor signals

---
+++ HelperGenerateSpeechDenoisingFeatures.m
@@ -0,0 +1,65 @@
+% Define a function for extracting the target and predictor magnitude STFT from the tall table
+function [targets, predictors] = HelperGenerateSpeechDenoisingFeatures(x, adsNoise, src)
+    
+    % Define system parameters for generating target and predictor signals
+    windowLength = 256;
+    win = hamming(windowLength,"periodic");
+    overlap = round(0.75 * windowLength);
+    ffTLength = windowLength;
+    inputFs = 48e3;
+    fs = 8e3;
+    numFeatures = ffTLength/2 + 1;
+    numSegments = 8;
+
+    % Make the audio length a multiple of the sample rate converter decimation factor.
+    decimationFactor = inputFs/fs;
+    L = floor(numel(x)/decimationFactor);
+    x = x(1:decimationFactor*L);
+    
+    % Convert the audio sample rate 
+    x = src(x);
+    reset(src)
+    
+    % Select a random noise sample
+    adsNoise = shuffle(adsNoise);
+    noise = read(adsNoise);
+    
+    % Set the audio samples to uniform length of 10 seconds
+    inputFs = 48000;
+    expected_length = 10;
+    if length(x) > expected_length * inputFs
+        x = x(1:expected_length * inputFs);
+    else
+        blankSignal = zeros(expected_length * inputFs - length(x),1);
+        x = [x; blankSignal];
+    end
+    
+    if length(noise) > expected_length * inputFs
+        noise = noise(1:expected_length * inputFs);
+    else
+        blankSignal = zeros(expected_length * inputFs - length(noise),1);
+        noise = [noise; blankSignal];
+    end
+    
+    % Set the noise power such that the signal-to-noise ratio (SNR) is zero dB
+    speechPower = sum(x.^2);
+    noisePower = sum(noise.^2);
+    noisyAudio = x + sqrt(speechPower/noisePower) * noise;
+    
+    % Generate magnitude STFT vectors from the original and noisy audio signals.
+    cleanSTFT = stft(x,'Window',win,'OverlapLength',overlap,'FFTLength',ffTLength);
+    cleanSTFT = abs(cleanSTFT(numFeatures-1:end,:));
+    noisySTFT = stft(noisyAudio,'Window',win,'OverlapLength',overlap,'FFTLength',ffTLength);
+    noisySTFT = abs(noisySTFT(numFeatures-1:end,:));
+    
+    % Generate the 8-segment training predictor signals from the noisy STFT.
+    noisySTFT = [noisySTFT(:,1:numSegments - 1), noisySTFT];
+    stftSegments = zeros(numFeatures, numSegments , size(noisySTFT,2) - numSegments + 1);
+    for index = 1:size(noisySTFT,2) - numSegments + 1
+        stftSegments(:,:,index) = (noisySTFT(:,index:index + numSegments - 1)); 
+    end
+    
+    % Set the targets and predictors.
+    targets = cleanSTFT;
+    predictors = stftSegments;
+end
---
+++ preprocessing_sample_add_noise.m
@@ -0,0 +1,39 @@
+% Import clean audio dataset
+dataFolder = "datasets_fullband/clean_fullband";
+adsTrain = audioDatastore(fullfile(dataFolder));
+
+reduceDataset = true;
+if reduceDataset
+    adsTrain = shuffle(adsTrain);
+    adsTrain = subset(adsTrain,1:1000);
+end
+
+% Import noise dataset
+noiseFolder = "datasets_fullband/noise_fullband";
+adsNoise = audioDatastore(fullfile(noiseFolder));
+
+% Define system parameters for generating target and predictor signals
+windowLength = 256;
+win = hamming(windowLength,"periodic");
+overlap = round(0.75 * windowLength);
+ffTLength = windowLength;
+inputFs = 48e3;
+fs = 8e3;
+numFeatures = ffTLength/2 + 1;
+numSegments = 8;
+
+% Create a sample rate converter to convert the 48 kHz audio to 8 kHz
+src = dsp.SampleRateConverter("InputSampleRate",inputFs, ...
+                              "OutputSampleRate",fs, ...
+                              "Bandwidth",8000);
+
+% Extract Features Using Tall Arrays
+T = tall(adsTrain);
+
+% Extract the target and predictor magnitude STFT from the tall table.
+% The function HelperGenerateSpeechDenoisingFeatures generates targets and
+% predictors at each cell
+[targets,predictors] = cellfun(@(x)HelperGenerateSpeechDenoisingFeatures(x,adsNoise,src),T,"UniformOutput",false);
+
+% Evaluate the targets and predictors.
+[targets,predictors] = gather(targets,predictors);
